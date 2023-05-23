b3f07b226a0065c199d71cd7969d9204305819ed
Danial Shakeri <dasha@Danials-MacBook-Pro.local>
2023-05-23 12:06:57 -0700

    Ran command script and got it working on seperate vm :D

--- deployment Scripts/CommandsRun.sh
+++ deployment Scripts/CommandsRun.sh
@@ -1,6 +1,6 @@
 # Assuming starting point is Java App
 # Most of this script comes from: https://github.com/selvasingh/tomcat-on-virtual-machine
-source ./deploymentScripts/setup-env-variables-on-azure-template.sh
+source ./deployment\ Scripts/setup-env-variables-on-azure-template.sh 
 
 az login 
 az account set --subscription ${SUBSCRIPTION}
@@ -15,6 +15,9 @@ az vm create \
 
 VM_IP_ADDRESS=`az vm show -d -g ${RESOURCE_GROUP} -n ${VM_NAME} --query publicIps -o tsv`
 
+# Run the following to open port 8080 on the vm since Tomcat uses it
+az vm open-port --port 8080 --resource-group ${RESOURCE_GROUP} --name ${VM_NAME} --priority 1100
+
 ssh ${ADMIN_USERNAME}@${VM_IP_ADDRESS}
 
 # adding a mix of this file on how to install tomcat https://www.digitalocean.com/community/tutorials/how-to-install-apache-tomcat-9-on-debian-10 
@@ -23,20 +26,8 @@ ssh ${ADMIN_USERNAME}@${VM_IP_ADDRESS}
 sudo apt update
 sudo apt install default-jdk
 sudo apt install maven
-        
-# Not needed for ubuntu
-        sudo apt install git
-        # Need to intstall mysql. Since we are using Debian we need to install gnupg first
-        # Following this doc: https://www.digitalocean.com/community/tutorials/how-to-install-the-latest-mysql-on-debian-10
-        sudo apt install gnupg
-        wget https://dev.mysql.com/get/mysql-apt-config_0.8.22-1_all.deb
 
-        # Follow the prompt here to install mysql server 8.0
-        sudo dpkg -i mysql-apt-config*
-        # update everything again 
-        sudo apt update
-
-# Install sql and don't enter a password to keep it simple for this app. May change later
+# Install sql
 sudo apt install mysql-server
 # Confirm sql is installed: 
 sudo systemctl status mysql
@@ -47,14 +38,12 @@ ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'SuperS3c
 flush privileges;
 exit;
 
-
 # Create a Tomcat group and user
 sudo groupadd tomcat
 sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
 
 # Install tomcat
 cd /tmp
-        sudo apt install curl
 curl -O https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.74/bin/apache-tomcat-9.0.74.tar.gz
 sudo mkdir /opt/tomcat
 sudo tar xzvf apache-tomcat-9*tar.gz -C /opt/tomcat --strip-components=1
@@ -78,9 +67,7 @@ sudo update-java-alternatives -l
 # Open a file called tomcat9.service in the /etc/systemd/system
 sudo nano /etc/systemd/system/tomcat.service
 
-
 # Add the following config to the file above: 
-
 [Unit]
 Description=Apache Tomcat Web Application Container
 After=network.target
@@ -107,7 +94,6 @@ Restart=always
 [Install]
 WantedBy=multi-user.target
 
-
 # Run the following to reload the systemd Daemon: 
 sudo systemctl daemon-reload
 
@@ -118,10 +104,8 @@ sudo systemctl status tomcat
 # Enable the service file so that Tomcat automatically starts at boot:
 sudo systemctl enable tomcat 
 
-# Run the following to open port 8080 on the vm to test if tomcat is working
-az vm open-port --port 8080 --resource-group ${RESOURCE_GROUP} --name ${VM_NAME} --priority 1100
 
-# GoTO http://[$VM_IP_ADDRESS]:8080/ to confirm that it's working :D :D :D 
+# Go TO http://[$VM_IP_ADDRESS]:8080/ to confirm that it's working :D :D :D 
 # Now we need to create a user with access to Tomcat Manager and Tools: by adding the following to 
 
 # Configure Tomcat 9
@@ -133,16 +117,6 @@ sudo nano /opt/tomcat/conf/tomcat-users.xml
     <role rolename="admin-gui"/>
     <role rolename="manager-script"/>
     <user username="manager" password="SuperS3cr3t" roles="manager-gui,admin-gui,manager-script"/>
-
-# Now we need to allow manager and host manager app connection coming from anywhere instead of just the server
-sudo nano /opt/tomcat/webapps/manager/META-INF/context.xml 
-
-# Here you want to comment out the following section by adding the <!-- -->
-<!-- <Valve className="org.apache.catalina.valves.RemoteAddrValve"
-         allow="127\.\d+\.\d+\.\d+|::1|0:0:0:0:0:0:0:1" /> -->
-
-#Comment out the same section here as well: 
-sudo nano /opt/tomcat/webapps/host-manager/META-INF/context.xml
 
 # Restart tomcat to make sure it's all working correctly
 sudo systemctl restart tomcat
@@ -158,14 +132,31 @@ sudo nano /usr/share/maven/conf/settings.xml
 
 # Now it's time to clone the project into the vm itself
 cd /opt
-sudo git clone https://github.com/dasha91/JavaDemoApp
+sudo git clone https://github.com/dasha91/JavaMavenApp
+cd JavaMavenApp
+# Run the following to give app access to create files/directories: 
+sudo chmod -R 777 /opt/JavaMavenApp/
 
+# Run and deploy the app
+mvn install tomcat7:deploy
 
+# Go TO http://[$VM_IP_ADDRESS]:8080/JavaMavenApp/Demo to confirm that it's working :D :D :D 
+
+######################################### Debugging ################################################
 # To debug and find logs on the machine go do: 
-
 vim /opt/tomcat/logs/catalina.out 
 
 # You may need to get god permissions virst by running: 
 sudo su
 
 
+######################################### Get access to tomcat manager from other device ################################################
+# Now we need to allow manager and host manager app connection coming from anywhere instead of just the server
+sudo nano /opt/tomcat/webapps/manager/META-INF/context.xml 
+
+# Here you want to comment out the following section by adding the <!-- -->
+<!-- <Valve className="org.apache.catalina.valves.RemoteAddrValve"
+         allow="127\.\d+\.\d+\.\d+|::1|0:0:0:0:0:0:0:1" /> -->
+
+#Comment out the same section here as well: 
+sudo nano /opt/tomcat/webapps/host-manager/META-INF/context.xml
